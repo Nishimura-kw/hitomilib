ae7c751c91199185480eb2d58d2f20829973ba68
Nishimura-kw <nishimura@mail.kywo.co.jp>
2020-05-01 13:54:38 +0900

    Exampleを消します

--- Example/Pods/Target Support Files/Pods-hitomilib_Tests/Pods-hitomilib_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_hitomilib_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_hitomilib_Tests
-@end
--- Example/Pods/Target Support Files/hitomilib/hitomilib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_hitomilib : NSObject
-@end
-@implementation PodsDummy_hitomilib
-@end
